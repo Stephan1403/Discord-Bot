d796c0badc152e630e6330ae072987e045643e69
Stephan1403 <Stephanf2003@gmail.com>
2021-03-16 11:07:18 +0100

    add public_channel

--- channelAdministration.py
+++ channelAdministration.py
@@ -1,11 +1,25 @@
-
+from voiceChannel import voiceChannel
+import random
 
 publicVoiceChannels = []
 
 
 #create Voice Channel
-async def create_voice_channel(member, voiceChannel):   #channel info passed as own type
-    channel = await member.guild.create_voice_channel(voiceChannel.name, category = voiceChannel.category, user_limit = voiceChannel.user_limit)
+async def create_voice_channel(member, name, category):   #channel info passed as own type
+    channel = await member.guild.create_voice_channel(name, category = category)
     return channel
 
+async def delete_voice_channel():
+    pass
 
+
+#user joined a voice channel
+    #depending wich he joined call function
+
+#public voice channel
+async def public_channel(member):
+    name =  f"Talk by {random.randint(1, 100)}"     #TODO: give numbers in order
+    category = "General"
+
+    channel = await create_voice_channel(member, name, category)
+    public_channel.append(voiceChannel(name, channel, member))
