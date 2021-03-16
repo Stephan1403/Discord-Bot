f8dc2d006a00976c51985a1876b39aa9013bb4fe
Stephan1403 <Stephanf2003@gmail.com>
2021-03-16 10:51:08 +0100

    created class voiceChannel

---
+++ voiceChannel.py
@@ -0,0 +1,6 @@
+class voiceChanel:
+    def __init__(self, name, index, user, channel) -> None:
+        self.name = name
+        self.index = index
+        self.user = user
+        self.channel = channel
