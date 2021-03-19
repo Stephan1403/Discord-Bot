84a573edf8104e85f3b0be1ca4cdbe27955ece79
Stephan1403 <Stephanf2003@gmail.com>
2021-03-19 17:44:28 +0100

    add textChannel

--- voiceChannel.py
+++ voiceChannel.py
@@ -1,5 +1,6 @@
 class voiceChannel:
-    def __init__(self, name, member, channel) -> None:
+    def __init__(self, name, member, channel, textChannel) -> None:
         self.name = name
         self.member = member
         self.channel = channel
+        self.textChannel = textChannel
