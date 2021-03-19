624f67a6f6111ac125f184b3bc2d0df0e55ab4dc
Stephan1403 <Stephanf2003@gmail.com>
2021-03-19 17:30:44 +0100

    add get_member_by_user

--- methods.py
+++ methods.py
@@ -1,3 +1,4 @@
+#category by name
 def get_category_by_name(guild, categoryName):
     category = None
     for c in guild.categories:
@@ -6,9 +7,17 @@ def get_category_by_name(guild, categoryName):
             break
     return category
 
+#member by user
+def get_member_by_user(guild, user):
+    member = None
+    for c in guild.members:
+        if c.name == user.name:
+            member = c
+            break
+    return member
 
 
-    #test varibales
+#test varibales
 def isInt(s):
     try:
         int(s)
