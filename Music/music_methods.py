88247c97975b2eeef6995a1fc9e096d52740fa2e
Stephan1403 <Stephanf2003@gmail.com>
2021-04-10 22:06:07 +0200

    Update music_methods.py
    instead of requests using youtube_search_requests

--- Music/music_methods.py
+++ Music/music_methods.py
@@ -1,33 +1,8 @@
-import requests
-from bs4 import BeautifulSoup
-from requests.models import stream_decode_response_unicode
-
-name = "Hallo"
-search = ""
+from youtube_search_requests import YoutubeSearch
 
 
-while "  " in name:
-    name.replace("  ", " ")
-
-for i in name.split(" "):
-    search = search + i + "+"
-
-
-
-url = f"https://www.youtube.com/results?name_query={search}"
-page = requests.get(url)
-soup = BeautifulSoup(page.content, 'html.parser')
-
-print(url)
-
-
-
-
-# payload = {'search': 'hallo Welt'}
-# r = requests.get("https://www.youtube.com/", params=payload)
-
-
-#receiving name, returning link
 def link_by_name(name):
-    pass
+    y = YoutubeSearch()
+    return y.search_videos(name, max_results=1)[0]['url']
 
+    #print(videos[0]['url'])
