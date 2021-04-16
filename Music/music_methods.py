b11c3b49d253e5968496932d854ac625c2594412
Stephan1403 <Stephanf2003@gmail.com>
2021-04-16 14:58:27 +0200

    rebuild

--- Music/music_methods.py
+++ Music/music_methods.py
@@ -1,4 +1,6 @@
+from os import link
 from youtube_search_requests import YoutubeSearch
+from youtube_dl import YoutubeDL
 
 
 def link_by_name(name):
@@ -6,3 +8,18 @@ def link_by_name(name):
     return y.search_videos(name, max_results=1)[0]['url']
 
     #print(videos[0]['url'])
+
+
+
+
+def get_link(name, url=True):
+
+
+
+    link = 'https://www.youtube.com/watch?v=9iHM6X6uUH8'
+    dl_opts = {'format': 'bestaudio'}       #best possible audio
+    with YoutubeDL(dl_opts) as ydl:
+        info = ydl.extract_info(link, download=False)   #dic with all infos
+        Url = info['formats'][0]['url']                 #url to audio file of video
+
+
