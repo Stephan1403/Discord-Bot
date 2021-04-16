c7ddba9d75c8cd0395d539343a27291ca4484704
Stephan1403 <Stephanf2003@gmail.com>
2021-04-16 17:39:07 +0200

    deleted

--- voiceChannel.py
+++
@@ -1,7 +0,0 @@
-class voiceChannel:
-    def __init__(self, name, member, channel, textChannel=None) -> None:
-        self.name = name
-        self.member = member
-        self.channel = channel
-        self.textChannel = textChannel
-        self.category = channel.category
