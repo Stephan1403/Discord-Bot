ab077c8678865b05e2d833342684aef2556910aa
Stephan1403 <Stephanf2003@gmail.com>
2021-04-17 20:08:30 +0200

    implemented music bot mehtods

--- cogs/Music/music_bot.py
+++ cogs/Music/music_bot.py
@@ -1,6 +1,13 @@
 import discord
 from discord.ext import commands
 
+#Music Bot imports
+from os import link
+from youtube_search_requests import YoutubeSearch
+from youtube_dl import YoutubeDL
+
+
+
 class music_bot(commands.Cog):
 
     def __init__(self, client):
@@ -11,9 +18,8 @@ class music_bot(commands.Cog):
         pass
 
     @commands.command()
-    async def cleara(self, ctx, amount=5):
-        #test if author = mod or higher
-        await ctx.channel.purge(limit=amount)
+    async def play(self, ctx, arg):
+        pass
 
 
 
@@ -21,3 +27,28 @@ def setup(client):
     client.add_cog(music_bot(client))
 
 
+
+#Music Bot methods
+async def join_channel(channel):
+    pass
+
+
+
+def Link_with_name(name):
+    y = YoutubeSearch()
+    return y.search_videos(name, max_results=1)[0]['url']
+
+
+def get_link(name, url=True):
+    
+    #link
+    if not url:
+        link = Link_with_name(name)
+    else: 
+        link = name         #ToDo Check validation
+
+    link = 'https://www.youtube.com/watch?v=9iHM6X6uUH8'
+    dl_opts = {'format': 'bestaudio'}       #best possible audio
+    with YoutubeDL(dl_opts) as ydl:
+        info = ydl.extract_info(link, download=False)   #dictonary with all infos
+        return info['formats'][0]['url']                 #url to audio file of video
