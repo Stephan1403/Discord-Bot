d0b67f2ef79ac2a3effe23f5badb976ac3da1a4e
Stephan1403 <Stephanf2003@gmail.com>
2021-04-22 10:40:01 +0200

    add intents

--- DiscordBot.py
+++ DiscordBot.py
@@ -1,7 +1,10 @@
 import os
+import discord
 from discord.ext import commands
 
-client = commands.Bot(command_prefix=".")
+
+intents = discord.Intents(messages = True, guilds = True, reactions = True, members = True, presences = True)
+client = commands.Bot(command_prefix=".", intents = intents)
 
 @client.event
 async def on_ready():
